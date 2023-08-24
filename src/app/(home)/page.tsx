ba6718edc9ea8ac3487e60f5d721c6a64ba1fa7f
Dennise Catolos <dentolos19@gmail.com>
2023-08-25 07:26:26 +0800

    Updated the backend

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,23 +4,23 @@ import { useStyles } from "@/lib/utilities";
 export default function Page() {
   const style = useStyles(styles);
   return (
-    <main className={style(["page"])}>
-      <div className={style(["container"])}>
-        <div className={style(["image"])}>
+    <main className={style("page")}>
+      <div className={style("container")}>
+        <div className={style("image")}>
           <img src={"https://gravatar.com/avatar/a8c63fb5500bc292955f80701cbf53c2?s=128"} />
         </div>
-        <div className={style(["content"])}>
-          <div className={style(["name"])}>Dennise Catolos</div>
-          <div className={style(["role"])}>Student @ Nanyang Polytechnic</div>
-          <div className={style(["socials"])}>
+        <div className={style("content")}>
+          <div className={style("name")}>Dennise Catolos</div>
+          <div className={style("role")}>Student @ Nanyang Polytechnic</div>
+          <div className={style("socials")}>
             <a href={"mailto:contact@dennise.me"}>
-              <i className={style(["lni", "lni-envelope"])}></i>
+              <i className={style("lni lni-envelope")}></i>
             </a>
             <a href={"https://github.com/dentolos19"}>
-              <i className={style(["lni", "lni-github-original"])}></i>
+              <i className={style("lni lni-github-original")}></i>
             </a>
             <a href={"https://linkedin.com/in/dentolos19"}>
-              <i className={style(["lni", "lni-linkedin-original"])}></i>
+              <i className={style(["lni lni-linkedin-original"])}></i>
             </a>
           </div>
         </div>
