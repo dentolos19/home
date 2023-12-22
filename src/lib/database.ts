1e880e1416713eae3ec43f3e1573bf9074033c85
Dennise Catolos <contact@dennise.me>
2023-12-22 21:39:21 +0800

    Migrated to Appwrite

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,15 +1,21 @@
-import PocketBase from "pocketbase";
-
-const pb = new PocketBase("https://easy-judge.pockethost.io");
+import { Client, Databases, Query } from "appwrite";
 
 export type Redirect = {
   name: string;
   destination: string;
 };
 
+const client = new Client();
+client.setEndpoint("https://cloud.appwrite.io/v1").setProject("65858f61bee3d52ba89c");
+const databases = new Databases(client);
+
 export async function getRedirect(name: string) {
   try {
-    return (await pb.collection("redirects").getFirstListItem(`name="${name}"`)) as Redirect;
+    return databases
+      .listDocuments("65858f87218d89759bf5", "65858f8d4f8acf2c8fcb", [Query.equal("name", name)])
+      .then((res) => {
+        return res.documents[0] as unknown as Redirect;
+      });
   } catch {
     return null;
   }
