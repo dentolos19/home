22decf1d24cf626f65657869a3fd6dfa4fb3a248
Dennise Catolos <contact@dennise.me>
2024-03-07 20:05:41 +0800

    Added project filter

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,13 +1,74 @@
 "use client";
 
 import { getRepos } from "@/github";
-import { use } from "react";
+import { use, useEffect, useState } from "react";
+
+const filters = [
+	{
+		name: "All",
+		topic: undefined,
+	},
+	{
+		name: ".NET",
+		topic: "dotnet",
+	},
+	{
+		name: "TypeScript",
+		topic: "web",
+	},
+	{
+		name: "Python",
+		topic: "python",
+	},
+  {
+		name: "Games",
+		topic: "game",
+	},
+  {
+		name: "VSCode Extensions",
+		topic: "vscode-extension",
+	},
+];
 
 export default function Page() {
 	const repos = use(getRepos("dentolos19"));
+
+	const [filteredRepos, setFilteredRepos] = useState(repos);
+	const [currentFilter, setCurrentFilter] = useState(filters[0]);
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		let filteredRepos = repos.filter(
+			(repo) => !repo.topics.includes("personal"),
+		);
+		if (currentFilter.topic) {
+			filteredRepos = repos.filter((repo) =>
+				repo.topics.includes(currentFilter.topic),
+			);
+			setFilteredRepos(filteredRepos);
+		} else {
+			setFilteredRepos(filteredRepos);
+		}
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [currentFilter]);
+
 	return (
 		<div className={"py-4 flex flex-col items-center gap-4"}>
-			{repos.map((repo) => (
+			<div className={"w-[90%] sm:w-[80%] flex gap-2 overflow-y-auto"}>
+				{filters.map((filter) => (
+					// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+					<span
+						key={filter.topic}
+						className={`px-3 py-2 min-w-fit text-xs rounded-xl cursor-pointer hover:bg-slate-700 ${
+							filter.topic === currentFilter.topic ? "bg-slate-500" : "bg-slate-800"
+						}`}
+						onClick={filter.topic === currentFilter.topic ? undefined : () => setCurrentFilter(filter)}
+					>
+						{filter.name}
+					</span>
+				))}
+			</div>
+			{filteredRepos.map((repo) => (
 				// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
 				<div
 					key={repo.full_name}
