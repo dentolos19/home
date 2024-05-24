486f92852891ec2d8e3948ecf6dc1620865ff77c
Dennise Catolos <dentolos19@gmail.com>
2024-05-24 14:39:02 +0800

    Updated project

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "gravatar.com",
-				port: "",
-				pathname: "/avatar/**",
-			},
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "gravatar.com",
+        port: "",
+        pathname: "/avatar/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
