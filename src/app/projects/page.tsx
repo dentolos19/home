f4cc5698cfbbdcc45851d56e37ea11faeab6f25e
Dennise Catolos <dentolos19@gmail.com>
2024-07-23 21:39:43 +0800

    Improved projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,20 +1,89 @@
 import { getRepos } from "@/content";
+import clsx from "clsx";
+import Link from "next/link";
 
-export default async function Page() {
-  const repos = await getRepos();
+const filters = [
+  {
+    label: "All",
+    topic: undefined,
+  },
+  {
+    label: ".NET (C#)",
+    topic: "dotnet",
+  },
+  {
+    label: "Web (JS/TS)",
+    topic: "web",
+  },
+  {
+    label: "Python",
+    topic: "python",
+  },
+  {
+    label: "Rust",
+    topic: "rust",
+  },
+  {
+    label: "Games",
+    topic: "game",
+  },
+  {
+    label: "VSCode Extensions",
+    topic: "vscode-extension",
+  },
+];
+
+export default async function Page(props: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+  const currentFilter =
+    filters.find((filter) => filter.topic === props.searchParams?.topic) ??
+    filters[0];
+  const repos = (await getRepos())
+    .filter((repo) => !repo.topics.includes("personal"))
+    .sort((a, b) => b.stargazers_count - a.stargazers_count)
+    .filter(
+      (repo) =>
+        !currentFilter.topic || repo.topics.includes(currentFilter.topic)
+    );
 
   return (
     <main className={"py-4"}>
-      <div className={"mx-auto w-[90%] md:w-[50%]"}>
-        <div className={"flex"}>{/* TODO */}</div>
+      <div className={"mx-auto w-[90%] md:w-[50%] space-y-2"}>
+        <div className={"flex"}>
+          {filters.map((filter) => (
+            <Link
+              key={filter.topic}
+              className={clsx(
+                "btn btn-sm",
+                filter.topic === currentFilter.topic && "btn-primary"
+              )}
+              href={filter.topic ? `?topic=${filter.topic}` : "/projects"}
+            >
+              {filter.label}
+            </Link>
+          ))}
+        </div>
         <div className={"flex flex-col gap-2"}>
           {repos.map((repo) => (
-            <div key={repo.full_name} className={"card bg-base-300"}>
+            <Link
+              key={repo.full_name}
+              className={"card cursor-pointer bg-base-300 hover:bg-base-200"}
+              href={repo.html_url}
+            >
               <div className={"card-body"}>
-                <h2 className={"card-title"}>{repo.name}</h2>
+                <h2 className={"card-title"}>
+                  {repo.name}
+                  {repo.stargazers_count > 0 && (
+                    <div className={"badge badge-accent gap-1"}>
+                      <i className={"fa-solid fa-star"} />
+                      {repo.stargazers_count}
+                    </div>
+                  )}
+                </h2>
                 <p>{repo.description}</p>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
