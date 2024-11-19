ebb60b330c0b14d273b8c4988547d917188ece2c
Dennise Catolos <dentolos19@gmail.com>
2024-11-19 09:09:51 +0800

    Updated stuff

--- src/app/(main)/blog/_components/post-item.tsx
+++ src/app/(main)/blog/_components/post-item.tsx
@@ -11,7 +11,11 @@ type PostItemProps = {
 
 export default function PostItem(props: PostItemProps) {
   return (
-    <Link className={"card cursor-pointer bg-base-300 transition hover:bg-base-200"} href={props.data.href}>
+    <Link
+      className={"card cursor-pointer bg-base-300 transition hover:bg-base-200"}
+      target={props.data.href && "_blank"}
+      href={props.data.href}
+    >
       <div className={"card-body"}>
         <h2 className={"card-title"}>{props.data.title}</h2>
         <p className={"text-xs text-gray-400"}>{props.data.date}</p>
